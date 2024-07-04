6ec1753cad344258e7b355e598c4d31c5050cb8e
asheekasamuels <samuelsasheeka133@gmail.com>
2024-07-04 11:56:07 +0200

    landing

--- src/components/LandingView.vue
+++ src/components/LandingView.vue
@@ -1,15 +1,30 @@
 <template>
-    <div>
-        <h1>Landing View</h1>
+    <div class="container landing">
+      <div class="row vh-100 align-items-center">
+        <div class="col-md-4">
+          <img src="https://asheekasamuels.github.io/All-Images/images/Asheeka1.jpg" alt="Asheeka1" class="img-fluid w-100 mb-5" loading="lazy">
+        </div>
+        <div class="col">
+          <div id="details">
+            <h1 class="display-1">Asheeka Samuels</h1>
+            <p v-if="title !== null">I am a <span> {{ title }} </span></p>
+            <Spinner v-else />
+          </div>
+        </div>
+      </div>
     </div>
-</template>
-
-<script>
-export default {
-
-}
-</script>
-
-<style>
-
-</style>
+  </template>
+  
+  <script setup>
+  import Spinner from './Spinner.vue';
+  import { computed, onMounted } from 'vue';
+  import { useStore } from 'vuex';
+  
+  const store = useStore();
+  const title = computed(() => store.state.title);
+  
+  onMounted(() => {
+    store.dispatch('fetchTitle');
+  });
+  </script>
+  
